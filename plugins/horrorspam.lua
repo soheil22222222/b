cefbb12013b79cc40a395fa1699416e4995b051b
samihacker <samsamsamramezani@gmail.com>
2016-04-06 08:51:21 -0700

    Update and rename fluxterspm.lua to horrorspam.lua

---
+++ plugins/horrorspam.lua
@@ -0,0 +1,22 @@
+do 
+
+function run(msg, matches)
+  return "🔸🔵HORROR Team is attacking🔵🔸🔵HORROR Team is attacking🔵🔸🔵HORROR Team is attacking🔵🔸🔵HORROR Team is attacking🔵🔸🔵HORROR Team is attacking🔵🔸🔵HORROR Team is attacking🔵🔸🔵HORROR Team is attacking🔵🔸🔵HORROR Team is attacking🔵🔸🔵HORROR Team is attacking🔵🔸🔵HORROR Team is attacking🔵🔸🔵HORROR Team is attacking🔵🔸🔵HORROR Team is attacking🔵🔸🔵HORROR Team is attacking🔵🔸🔵HORROR Team is attacking🔵🔸🔵HORROR Team is attacking🔵🔸🔵HORROR Team is attacking🔵🔸🔵HORROR Team is attacking🔵🔸🔵HORROR Team is attacking🔵🔸🔵HORROR Team is attacking🔵🔸🔵HORROR Team is attacking🔵🔸🔵HORROR Team is attacking🔵🔸🔵HORROR Team is attacking🔵🔸🔵HORROR Team is attacking🔵🔸🔵HORROR Team is attacking🔵🔸🔵HORROR Team is attacking🔵🔸🔵HORROR Team is attacking🔵🔸🔵HORROR Team is attacking🔵🔸🔵HORROR Team is attacking🔵🔸🔵HORROR Team is attacking🔵🔸🔵HORROR Team is attacking🔵🔸🔵HORROR Team is attacking🔵🔸🔵HORROR Team is attacking🔵🔸🔵HORROR Team is attacking🔵🔸🔵HORROR Team is attacking🔵🔸🔵HORROR Team is attacking🔵🔸🔵HORROR Team is attacking🔵🔸🔵HORROR Team is attacking🔵🔸🔵HORROR Team is attacking🔵🔸🔵HORROR Team is attacking🔵🔸🔵HORROR Team is attacking🔵🔸🔵HORROR Team is attacking🔵🔸🔵HORROR Team is attacking🔵🔸🔵HORROR Team is attacking🔵🔸🔵HORROR Team is attacking🔵🔸🔵HORROR Team is attacking🔵🔸🔵HORROR Team is attacking🔵🔸🔵HORROR Team is attacking🔵🔸🔵HORROR Team is attacking🔵🔸🔵HORROR Team is attacking🔵🔸🔵HORROR Team is attacking🔵🔸🔵HORROR Team is attacking🔵🔸🔵HORROR Team is attacking🔵🔸🔵HORROR Team is attacking🔵🔸🔵HORROR Team is attacking🔵
+  end
+  
+  return {
+  description = "a plugin for HORROR Team", 
+  usage = {
+  "!HORROR Team : horror spam",
+  "horrorpm : HORROR Team spam",
+  patterns = {
+    "^horrorfuck$",
+	"^[Hh][Oo][Rr][Rr][Oo][Rr][Ff][Uu][Cc][Kk]$",
+
+  },
+  run = run
+}
+
+end
+  
+
