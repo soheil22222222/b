a54c7b4de5970d61786d343062cbb601e76ce607
samihacker <samsamsamramezani@gmail.com>
2016-04-07 01:41:42 -0700

    Update xy4.lua

--- plugins/xy4.lua
+++ plugins/xy4.lua
@@ -1,6 +1,6 @@
 do
 local function run(msg, matches)
-local bot_id = 212833120-- آیدی اینوایت کننده رو اینجا بنویسید --
+local bot_id = 207248520-- آیدی اینوایت کننده رو اینجا بنویسید --
 local fbot1 = 125871286-- آیدی اینوایت شونده را اینجا بنویسید --
     if msg.action.type == "chat_del_user" and msg.action.user.id == tonumber(fbot1) then
        chat_add_user("chat#id"..msg.to.id, 'user#id'..fbot1, ok_cb, false)
